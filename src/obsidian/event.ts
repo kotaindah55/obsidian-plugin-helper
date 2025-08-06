651a2a79a1eb306858ac07efda940433a2b9b47c
kotaindah55 <kotaindah55@gmail.com>
2025-08-07 01:03:02 +0300

    feat: add `EmitterComponent`
    It is combination of `Component` and `Events`

--- src/obsidian/event.ts
+++ src/obsidian/event.ts
@@ -1,4 +1,50 @@
-import type { EventRef } from 'obsidian';
+import { Component, type EventRef, Events } from 'obsidian';
+import type { Except } from 'type-fest';
+
+/**
+ * Component that acts as an event emitter, inheriting {@link Events | `Events`}
+ * functionality.
+ */
+export class EmitterComponent extends Component implements Except<Events, '_'> {
+	protected readonly emitter: Events;
+
+	constructor() {
+		super();
+		this.emitter = new Events();
+	}
+
+	public on(name: string, callback: (...data: any[]) => unknown, ctx?: unknown): EventRef {
+		return this.emitter.on(name, callback, ctx);
+	}
+
+	public off(name: string, callback: (...data: unknown[]) => unknown): void {
+		return this.emitter.off(name, callback);
+	}
+
+	public offref(ref: EventRef): void {
+		return this.emitter.offref(ref);
+	}
+
+	public trigger(name: string, ...data: unknown[]): void {
+		return this.emitter.trigger(name, ...data);
+	}
+
+	public tryTrigger(evt: EventRef, args: unknown[]): void {
+		return this.emitter.tryTrigger(evt, args);
+	}
+
+	/**
+	 * Similiar to {@link on | `on()`}, but the handler will be detached
+	 * after the first call.
+	 */
+	public once(name: string, callback: (...data: any[]) => unknown, ctx?: unknown): void {
+		let ref = this.on(name, (...data) => {
+			callback.apply(ctx, data);
+			this.offref(ref);
+		}, ctx);
+	}
+}
+
 
 /**
  * Place the event handler at the highest precedence, so it will be
